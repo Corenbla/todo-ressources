9856b3d76a4ccc7dc8c8a8b8846aa94618619600
Corentin <corenbla@gmail.com>
2020-10-13 12:22:09 +0200

    API PUT 'api/v1/list/:id'

--- node-sample-app/app/lists.js
+++ node-sample-app/app/lists.js
@@ -12,7 +12,7 @@ router.get('/', async (req, res) => {
         return res.status(500).json(e);
     }
 
-    return res.status(200).json({lists: lists});
+    return res.status(200).json(lists);
 });
 
 // GET by ID
@@ -24,37 +24,65 @@ router.get('/:id', [param('id').isInt({min: 0})], async (req, res) => {
 
     const {id} = req.params;
 
-    let item;
+    let list;
 
     try {
-        item = await knex('list').where({id: id});
+        list = await knex('list').where({id: id});
     } catch (e) {
         return res.status(500).json(e);
     }
 
-    if (!item.length) {
+    if (!list.length) {
         return res.sendStatus(404);
     }
 
-    return res.status(200).json(item[0]);
+    return res.status(200).json(list[0]);
 });
 
 // POST Create list
-router.post('/',
-    [
-        body('title').isString(),
-    ],
-    async (req, res) => {
-        const list = {
-            title: req.body.title,
-        };
+router.post('/', [body('title').isString()], async (req, res) => {
+    const list = {
+        title: req.body.title,
+    };
 
-        try {
-            await knex('list').insert(list);
-        } catch (e) {
-            return res.status(500).json(e);
-        }
+    try {
+        await knex('list').insert(list);
+    } catch (e) {
+        return res.status(500).json(e);
+    }
 
-        return res.sendStatus(201);
-    });
+    return res.sendStatus(201);
+});
+
+// PUT Update list
+router.put('/:id', [
+    param('id').isInt({min: 0}),
+    body('title').isString(),
+], async (req, res) => {
+    const {id} = req.params;
+    const listUpdated = {
+        title: req.body.title,
+    };
+
+    let list;
+
+    try {
+        list = await knex('list').where({id: id});
+    } catch (e) {
+        return res.status(500).json(e);
+    }
+
+    if (!list.length) {
+        return res.sendStatus(404);
+    }
+
+    try {
+        await knex('list').update(listUpdated).where({id: id});
+    } catch (e) {
+        return res.status(500).json(e);
+    }
+
+    return res.sendStatus(200);
+});
+
 module.exports = router;
