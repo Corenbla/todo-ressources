5c002cd14ae3d2600cb25ec9aa3091fc49b5cfef
Corentin <corenbla@gmail.com>
2020-10-13 16:30:37 +0200

    PUT Check task

--- node-sample-app/app/tasks.js
+++ node-sample-app/app/tasks.js
@@ -38,4 +38,36 @@ router.put('/:id', [
     return res.sendStatus(200);
 });
 
+// PUT Check task
+router.put('/:id/check', [
+    param('id', 'Expected positive integer for param : id').isInt({min: 0}),
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const {id} = req.params;
+
+    let task;
+
+    try {
+        task = await knex('task').where({id: id});
+    } catch (e) {
+        return res.status(500).json(e);
+    }
+
+    if (!task.length) {
+        return res.sendStatus(404);
+    }
+
+    try {
+        await knex('task').update({done: ! task.done}).where({id: id});
+    } catch (e) {
+        return res.status(500).json(e);
+    }
+
+    return res.sendStatus(200);
+});
+
 module.exports = router;
