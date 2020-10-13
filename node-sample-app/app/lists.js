a0466cb7cbad63bd121a0e7b782035172d26d8eb
Corentin <corenbla@gmail.com>
2020-10-13 15:39:30 +0200

    POST Create task

--- node-sample-app/app/lists.js
+++ node-sample-app/app/lists.js
@@ -145,4 +145,32 @@ router.get('/:id/task', [param('id', 'Expected positive integer for param : id')
     return res.status(200).json(tasks);
 });
 
+// POST create task
+router.post('/:id/task',
+    [
+        body('title', 'Expected string for key : title').isString(),
+        param('id', 'Expected positive integer for param : id').isInt({min: 0}),
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({errors: errors.array()});
+        }
+
+        const task = {
+            title: req.body.title,
+            list_id: req.params.id,
+            done: false,
+        };
+
+        try {
+            await knex('task').insert(task);
+        } catch (e) {
+            return res.status(500).json(e);
+        }
+
+        return res.sendStatus(201);
+    }
+);
+
 module.exports = router;
